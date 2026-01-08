7fa4ad5579c31da20b7ab5c1403273b5e9ebe3b4
Carl Saenz <technojunky@outlook.com>
2026-01-08 17:21:10 +0000

    update data source

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,12 @@
 resource "aws_instance" "example" {
   ami           =  var.AMI_ID
   instance_type = var.VM_Size
+
+vpc_security_group_ids = [
+  aws_security_group.allow_tls.id
+]
   tags = {
     Name = var.VM_Name
   }
 }
+
